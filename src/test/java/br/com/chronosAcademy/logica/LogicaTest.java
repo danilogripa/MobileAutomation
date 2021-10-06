3765bb44a1407c82f6b0ec04e233142a0ff7c757
Danilo Gripa <danilogripa@gmail.com>
2021-10-06 15:09:18 -0300

    adding capabilities and running calculator execution

---
+++ src/test/java/br/com/chronosAcademy/logica/LogicaTest.java
@@ -0,0 +1,13 @@
+package br.com.chronosAcademy.logica;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicaTest {
+    @Test
+    public void validaTerrenoGrande(){
+        Logica logica = new Logica();
+        assertEquals("Terreno Grande", logica.getTipoTerreno(101));
+    }
+}
